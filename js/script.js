8ae2ae5225781af94fae07ffae94dcd5f277960c
Irwing <ivielma101@gmail.com>
2018-12-10 02:57:50 +0000

    RPS project

--- js/script.js
+++ js/script.js
@@ -4,9 +4,42 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userChoice;
+userChoice="";
+var computerChoice;
+computerChoice="";
+var winner;
+winner="";
+var randomNumber;
+
+randomNumber=0;
+$("#shoot").click(function(){
+    userChoice=$("#input").val();
+    $("#userChoice").text(userChoice);
+    randomNumber=Math.random();
+    if( randomNumber<.3333333333333333333333333){
+        computerChoice="Rock";
+    }
+    else if( randomNumber<.666666666666666666666667){
+        computerChoice="Scissors";
+    }
+    else{
+        computerChoice:"Paper";
+    }
+
+});
+    computerChoice=$("#input").val();
+    $("#computerChoice").text(computerChoice)
+var userInput=$("#userChoice").val();
+var computerInput=$("#computerChoice").val();
+
+var computerWinner= (computerChoice=== computerInput);
+var userWinner
+
 $("button").click(function(){
 $("#userChoice").text($("#input").val());
 });
+
 
 
 // DOCUMENT READY FUNCTION BELOW
